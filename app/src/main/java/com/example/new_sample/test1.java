aa2efab199c7ff685403a16e4a907d280d1d1db6
mahamud <avmahmud0@gmail.com>
2023-03-22 04:43:42 +0600

    create test1

--- app/src/main/java/com/example/new_sample/test1.java
+++ app/src/main/java/com/example/new_sample/test1.java
@@ -1,2 +1,31 @@
-package com.example.new_sample;public class test1 {
+package com.example.new_sample;
+
+public class test1 {
+    private string name;
+    private int age;
+    private int roll;
+
+    public string getName() {
+        return name;
+    }
+
+    public void setName(string name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public int getRoll() {
+        return roll;
+    }
+
+    public void setRoll(int roll) {
+        this.roll = roll;
+    }
 }
