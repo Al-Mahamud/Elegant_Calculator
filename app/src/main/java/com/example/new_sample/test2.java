8557d363763627037eb6de0abb0b8c5f125931ca
mahamud <avmahmud0@gmail.com>
2023-03-23 02:15:24 +0600

    create a variable a

--- app/src/main/java/com/example/new_sample/test2.java
+++ app/src/main/java/com/example/new_sample/test2.java
@@ -3,6 +3,15 @@ package com.example.new_sample;
 public class test2 {
     private string name;
     private int roll;
+    private int a;
+
+    public int getA() {
+        return a;
+    }
+
+    public void setA(int a) {
+        this.a = a;
+    }
 
     public string getName() {
         return name;
