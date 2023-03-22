107f42674c5db4bc2077aa1ea5f168af95e33e0a
mahamud <avmahmud0@gmail.com>
2023-03-23 02:14:04 +0600

    create a java class

---
+++ app/src/main/java/com/example/new_sample/test2.java
@@ -0,0 +1,22 @@
+package com.example.new_sample;
+
+public class test2 {
+    private string name;
+    private int roll;
+
+    public string getName() {
+        return name;
+    }
+
+    public void setName(string name) {
+        this.name = name;
+    }
+
+    public int getRoll() {
+        return roll;
+    }
+
+    public void setRoll(int roll) {
+        this.roll = roll;
+    }
+}
